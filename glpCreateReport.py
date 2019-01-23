ef58f5323fd066cad751d3f523b2a436f2de9388
JCSheeron <jcs@san.rr.com>
2019-01-22 16:09:57 -0800

    rolling on...

--- glpCreateReport.py
+++ glpCreateReport.py
@@ -8,7 +8,34 @@
 # This program will import a data from a file exported by the tester. It will
 # create a PDF file. The PDF file will contain numerical data in easy to read
 # format, and it will also plot the data.
-# 
+#
+# **** Program operation
+#   1)  The program will read in a config file. The file is either specified by
+#       the -c/--configFile command line argument, or defaults to config.ini if not
+#       specified.  The config file specifies:
+#           a. The data file path
+#           b. The test definition path
+#
+#   2)  The program will read in one or more test files from the test definition
+#       path specified in the config file.  If a file is specified by the
+#       -t/--testDfnFile command line option, then this file will be searched
+#       for and loaded. If this file is not present, an error will result. If no
+#       file is specified, then all the test definitions (*.TPR) will be loaded, and
+#       the test data will determine which to use (below) by searching for the
+#       proper test id.
+#
+#   3)  The program will read in and parse one or more data files from the data
+#       path specified in the config file. If a file is specified by the
+#       -d/--dataFile command line option, then this file will be loaded and
+#       used.  If this file is not present, an error will result. If no file
+#       specified, thn all the data files (*.csv) will be loaded.
+#
+#   4)  Once one or more data files are loaded, the program will ensure that it
+#       has available to it (from step 2), the corresponding test definition. The
+#       test ID is used.
+#
+#   5)  TODO: What else
+#
 # TODO: More info about the input file format.
 # TODO: More info about the text data in the output PDF.
 # TODO: More info about the plot in the output PDF.
@@ -18,18 +45,16 @@
 #
 # imports
 #
-# system related
-# import sys
 # date and time stuff
 from datetime import datetime, time
-# from pandas.tseries.frequencies import to_offset
-# from dateutil import parser as duparsr
+
+# os file related
+# join combines path stirngs in a smart way (i.e. will insert '/' if missing,
+# or remove a '/' if a join creates a repeat.
+from os.path import join
 
 # config file parser
 import configparser
-
-# csv file stuff
-import csv
 
 # arg parser
 import argparse
@@ -51,9 +76,9 @@ from PyPDF2 import PdfFileMerger, PdfFileReader
 # TimeStamped Indexed Data Class
 # from bpsTsIdxData import TsIdxData
 # list duplication helper functions
-# from bpsListDuplicates import listDuplicates 
+# from bpsListDuplicates import listDuplicates
 # from bpsListDuplicates import listToListIntersection
-
+from bpsFile import listFiles
 
 # **** argument parsing
 # define the arguments
@@ -67,28 +92,59 @@ a Schleich GLP2-ce Hi Pot Modular Tester. """
 
 parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, \
                                  description=descrStr, epilog=eplStr)
-parser.add_argument('dataFileName', help='Input data file (Schleich GLP2-ce file)')
-parser.add_argument('outputFileName', help= 'Output PDF file name. The .pdf extension \
+parser.add_argument('outputFile', help= 'Output PDF file name. The .pdf extension \
 will be added to the file name if not specified.')
+parser.add_argument('-d', '--dataFile', default='', metavar='', \
+                    help='Input data file (Schleich GLP2-ce file, *.csv). If \
+specified, this data file will be used. If not specified, all *.csv files present \
+in the data_dir path specified in the config directory will be used.')
+parser.add_argument('-de', '--dataFileEncoding', default='UTF-16', metavar='', \
+                    help='Data file encoding. Default is UTF-16.')
 parser.add_argument('-dirPrefix', default='', metavar='', \
                     help='Directory prefix. If specified, this is prepended to \
-the paths specified in the configuration file (default is config.ini). This is \
+the paths specified in the configuration ini file (default is config.ini). This is \
 helpful if the data is inside directories that are named using serial numbers, \
 dates, times, test numbers, station numbers, or some other dynamic data that is \
-not known when the config file is created.')
+not known when the configuration file is created.')
 parser.add_argument('-c', '--configFile', default='config.ini', metavar='', \
-                   help='Config file. Default is config.ini.')
+                   help='Config file. Default is config.ini. A list of files \
+may be specified ([\'file1.ini\',\'file2.ini\',...]) if the configuration is \
+spread across multiple files. The resuling configuration will be a union of the \
+information, so if keys are repeated, the key will have the value it had in the \
+last file read.')
+parser.add_argument('-ce', '--configEncoding', default='UTF-8', metavar='', \
+                   help='Config file encoding. Default is UTF-8.')
+parser.add_argument('-t', '--testDfnFile', default='', metavar='', \
+                   help='Test definition file (*.TPR). If specified, this and only \
+this test definition is used. If not specified, all *.TPR files in the test_dfn_dir \
+will be considered.  A proper data file will include a test definiton id. In \
+either case (specified or not), the test id must match test id in the data file. \
+In the non-specified case, any files present will be searched until the test id \
+in the data is found in one of the test definition files.')
+parser.add_argument('-te', '--testDfnEncoding', default='UTF-16', metavar='', \
+                   help='Test definition file encoding. Default is UTF-16.')
 parser.add_argument('-v', '--verbose', action='store_true', default=False, \
                     help='Verbose output, usually used for troubleshooting.')
 # parse the arguments
 args = parser.parse_args()
 
 # At this point, the arguments will be:
-# Argument          Values      Description
-# args.dataFileName     string
-# args.outputFileName   string
-# args.configFile       string, default 'config.ini'
-# args.verbose          True/False, default False
+# Argument          Values       Description
+# args.outputFile       string   Output file name. Required.
+# args.dataFile         string   Input file name. Optional. Data directory in
+#                                config file will be searched if a file is not
+#                                specified.
+# args.dirPrefix        string   Optional. Prepended to directory path specified
+#                                in config file.
+# args.configFile       string   Optional. Default 'config.ini'
+# args.configEncoding   string   Optional, Default 'UTF-8'
+# args.testDfnFile      string   Optional. Test Definition File. If a file is
+#                                not specified, all *.TPR files int the path
+#                                specified in the config file will be considered.
+# args.testDfnEncoding  string   Optional. Default 'UTF-16'. The tester uses
+#                                UTF-16 encoding when creating these definition
+#                                files.
+# args.verbose          True/False, default False. Increase output messages.
 
 # Put the begin mark here, after the arg parsing, so argument problems are
 # reported first.
@@ -107,18 +163,44 @@ if not cfgFile:
     quit()
 # if we get here, we have config data
 if args.verbose:
-    print('The following config file(s) are used:')
+    print('\nThe following config file(s) are used:')
     print(cfgFile)
-    print('\nThe resulting configuration has these settings:')
+    print('The resulting configuration has these settings:')
     for section in config:
         print(section)
         for key in config[section]:
             print('  ', key, ':', config[section][key])
+# set the data paths to use internally
+if args.dirPrefix is not None:
+    dataPath = join(args.dirPrefix, config['Paths']['common_dir'], config['Paths']['data_dir'])
+    testDfnPath = join(args.dirPrefix, config['Paths']['common_dir'], config['Paths']['test_dfn_dir'])
+else:
+    dataPath = join(config['Paths']['common_dir'], config['Paths']['data_dir'])
+    testDfnPath = join(config['Paths']['common_dir'], config['Paths']['test_dfn_dir'])
 
+if args.verbose:
+    print('\nTest Data Path: ' + dataPath)
+    print('Test Definition Path: ' + testDfnPath)
 
+# Get a list of test definition files in the test definition path
+testDfns = listFiles(testDfnPath)
 
+if args.verbose:
+    print('\nTest Definitions found:')
+    print(testDfns)
 
-#get end  processing time
+# If a test definition file was specified, see if it was found. If not, error out.
+if args.testDfnFile != '' and args.testDfnFile is not None and not args.testDfnFile in testDfns:
+    print('\nERROR: The test definition file \'' + args.testDfnFile + '\' was specified, \
+but was not found. Exiting.')
+    quit()
+elif args.testDfnFile != '' and args.testDfnFile is not None and args.testDfnFile in testDfns:
+    print('\nSepecified test definition file \'' + args.testDfnFile + '\' was found and is being used.')
+elif args.testDfnFile == '' or args.testDfnFile is None:
+    print('\nThere was no test definition file specified.  The test definition id in the \
+data will be used to try and determne the correct test definition file to use.')
+
+# get end  processing time
 procEnd = datetime.now()
 print('\n**** End Processing ****')
 print('    Process end time: ' + procEnd.strftime('%m/%d/%Y %H:%M:%S'))
